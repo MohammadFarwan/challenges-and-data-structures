f7ba6f001d1e45828dc5c14a5d46bd451909ea21
MohammadFarwan <alfrwan58@gmail.com>
2024-07-12 23:58:25 +0300

    linkedList

---
+++ Data-Structures/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(5);
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+
+            // Print all nodes
+            list.PrintList();
+
+            // Remove a node
+            list.Remove(10);
+            list.PrintList();
+
+            Console.WriteLine(list.Includes(5));
+            Console.WriteLine(list.Includes(100)); 
+        }
+    }
+
+}
